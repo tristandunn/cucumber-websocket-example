591977c1174e6a4e1adffa18e8244f6160e9f982
Tristan Dunn <tristanzdunn@gmail.com>
2011-02-16 19:25:53 -0500

    Ensure the socket is connected before continuing.

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -3,5 +3,9 @@ Given /^a user named "([^"]*)" is online$/ do |name|
     Given %{I am on the homepage}
     When  %{I fill in "Name" with "#{name}"}
     And   %{I submit the new user form}
+
+    wait_until(5) do
+      page.evaluate_script("(function() { return socket.readyState == 1; })();")
+    end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,8 @@
-$(function() {
-  var
-  socket = new WebSocket('ws://localhost:8080/');
-  socket.onmessage = function(event) {
-    var message = $.parseJSON(event.data);
+var
+socket = new WebSocket('ws://localhost:8080/');
+socket.onmessage = function(event) {
+  var message = $.parseJSON(event.data);
 
-    $('#events table')
-      .append('<tr><td>' + message.name + ' has entered the room.</td></tr>');
-  };
-});
+  $('#events table')
+    .append('<tr><td>' + message.name + ' has entered the room.</td></tr>');
+};
