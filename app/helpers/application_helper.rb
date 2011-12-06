64ff4cc1e510d400bcaacafb0ad559cca0161936
Tristan Dunn <tristanzdunn@gmail.com>
2011-12-06 15:28:38 -0600

    Upgrade jQuery version.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def javascript_include_tag_for_jquery
     if Rails.env.production?
-      javascript_include_tag 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'
+      javascript_include_tag 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'
     else
-      javascript_include_tag 'vendor/jquery-1.6.4.min.js'
+      javascript_include_tag 'vendor/jquery-1.7.1.min.js'
     end
   end
 
